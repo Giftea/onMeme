777cff2221ae9744976ae35be67807622a042abc
Giftea <eleojogiftuhiene@gmail.com>
2025-02-08 09:13:08 +0100

    fetch address

--- components/Text.tsx
+++ components/Text.tsx
@@ -1,12 +1,13 @@
-"use client"
-import { useAddress } from '@/hooks'
-import React from 'react'
+"use client";
+import { useAddress } from "@/hooks";
+import React from "react";
 
-export default function Text({ initialAddress }) {
-    const { data: userId } = useAddress(initialAddress)
+export default function Text({
+  initialAddress,
+}: {
+  initialAddress: string | null;
+}) {
+  const { data: userId } = useAddress(initialAddress);
 
-    console.log(userId)
-  return (
-    <div>Text</div>
-  )
+  return <div>{userId} </div>;
 }
