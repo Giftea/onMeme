9e5318e8b3c539e48bd0567a49ccd74a5355bd07
clarencepenz <clarencedespenz@gmail.com>
2025-02-20 23:56:06 +0100

    chore: refactor component to only use trpc

--- components/profile/index.tsx
+++ components/profile/index.tsx
@@ -2,30 +2,20 @@
 import UserMemesTemplate from "@/components/profile/UserTabsTemplate";
 import UserMemes from "@/components/profile/UserMemes";
 import { trpc } from "@/lib/trpc.utils";
-import { useEffect, useState } from "react";
 import UserNFTs from "./UserNFTs";
-import { NFT } from "@/lib/types";
+import React from "react";
 
 export default function Page({ address }: { address: string | null }) {
-  const [nfts, setNFTs] = useState<NFT[]>();
-
-  const { data: nftsData, isLoading: isNFTsLoading } =
-    trpc.nft.getNFTsByOwner.useQuery({
-      owner: String(address),
-    });
-
-  useEffect(() => {
-    if (nftsData) {
-      setNFTs(nftsData as NFT[]);
-    }
-  }, [isNFTsLoading, nftsData]);
+  const { data, isLoading } = trpc.nft.getNFTsByOwner.useQuery({
+    owner: String(address),
+  });
 
   return (
-    <>
+    <React.Fragment>
       <UserMemesTemplate
-        nfts={<UserNFTs isLoading={isNFTsLoading} nfts={nfts} />}
+        nfts={<UserNFTs isLoading={isLoading} nfts={data} />}
         memes={<UserMemes address={address} />}
       />
-    </>
+    </React.Fragment>
   );
 }
