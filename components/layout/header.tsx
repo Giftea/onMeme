dda6364dde69c100984bcfb2f49aaa1e9ac2b419
Giftea <eleojogiftuhiene@gmail.com>
2025-02-25 19:13:49 +0100

    chore: rmv unused import

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { trpc } from "@/lib/utils/trpc.utils";
-import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
