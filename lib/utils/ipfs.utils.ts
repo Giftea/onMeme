b3d8a2d79a7c02a2c10c448ed6a48c8fccfd5383
clarencepenz <clarencedespenz@gmail.com>
2025-02-27 13:43:14 +0100

    chore: remove console.log

--- lib/utils/ipfs.utils.ts
+++ lib/utils/ipfs.utils.ts
@@ -14,8 +14,6 @@ export async function uploadToIpfs(url: string, server: boolean) {
     body: formData,
   });
 
-  console.log("uploadResponse", uploadResponse);
-
   const ipfsUrl = (await uploadResponse.json()) as string;
 
   return ipfsUrl;
