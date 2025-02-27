c2874cdf244cdb20487aafab5ae6ce4ae30a1c05
clarencepenz <clarencedespenz@gmail.com>
2025-02-27 13:47:06 +0100

    chore: clean up

--- lib/utils/ipfs.utils.ts
+++ lib/utils/ipfs.utils.ts
@@ -1,4 +1,4 @@
-export async function uploadToIpfs(url: string, server: boolean) {
+export async function uploadToIpfs(url: string, isServer: boolean) {
   const res = await fetch(url);
 
   const blob = await res.blob();
@@ -7,7 +7,7 @@ export async function uploadToIpfs(url: string, server: boolean) {
   const formData = new FormData();
   formData.append("file", file);
 
-  const APP_URL = server ? `${process.env.APP_URL}/api/files` : " /api/files";
+  const APP_URL = isServer ? `${process.env.APP_URL}/api/files` : " /api/files";
 
   const uploadResponse = await fetch(APP_URL, {
     method: "POST",
